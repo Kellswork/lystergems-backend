8227dafd74be6d71c23668a0af74f7e95d5b64a4
kellswork <Bighead93+>
2020-03-30 14:13:44 +0100

    create knexfile.js, set up environment connections

---
+++ knexfile.js
@@ -0,0 +1,53 @@
+// Update with your config settings.
+
+module.exports = {
+//npx knex init if knex is not installed globally
+// once this file is setup, we move to migrations
+  development: {
+    client: 'pg',
+    connection: process.env.DEV_URL,
+    migrations: {
+      directory: './db/migrations'
+    },
+    seeds: {
+      directory: './db/seeds/dev'
+    },
+    useNullAsDefault: true
+  },
+  testing: {
+    client: 'pg',
+   connection: process.env.TEST_URL,
+    migrations: {
+      directory: './db/migrations'
+    },
+    seeds: {
+      directory: './db/seeds/test'
+    },
+    useNullAsDefault: true
+  },
+
+  staging: {
+    client: 'pg',
+   connection: process.env.STAG_URL,
+    migrations: {
+      directory: './db/migrations'
+    },
+    seeds: {
+      directory: './db/seeds/staging'
+    },
+    useNullAsDefault: true
+  },
+
+  production: {
+    client: 'pg',
+    connection: process.env.PROD_URL,
+    migrations: {
+      directory: '/.db/migrations'
+    },
+    seeds: {
+      directory: '/.db/seeds/production'
+    },
+    useNullAsDefault: true
+  },
+
+};
