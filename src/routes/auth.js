9478f82b1d21f6fb269ba78b0c5a91c101e43ade
Omenkish <omenkish@gmail.com>
2020-04-08 16:49:15 +0100

    Setup domain/feature based file system and install cors and body-parser

--- src/routes/auth.js
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import { register } from '../controllers/authController';
-
-const router = express.Router();
-
-router.post('/register', signUpValidator, register);
-
-export default router;
--- src/server.js
+++ src/server.js
@@ -1,14 +1,23 @@
 import 'core-js/stable';
 import express from 'express';
+import cors from 'cors';
+import bodyParser from 'body-parser';
+
+import routes from './resources/routesIndex';
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 app.get('/', (req, res) =>
   res.status(200).json({
     message: 'Welcome to Kells shopping site',
   }),
 );
+app.use('/api/v1', routes);
 
 export const server = () => {
   app.listen(PORT, () => console.log(`app listening on port ${PORT}!`));
