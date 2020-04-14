cd751396cade2138fcfd18e58515ce2f642919d2
kellswork <Bighead93+>
2020-04-15 00:36:10 +0100

    add user validation using express-validator

---
+++ src/middlewares/validateUser.js
@@ -0,0 +1,68 @@
+const { check, body, validationResult } = require('express-validator');
+
+const validateUser = [
+  check('firstname')
+    .matches(/^[a-zA-Z]+$/i)
+    .withMessage('firstname must contain only alphabets')
+    .isLength({
+      min: 1,
+    })
+    .withMessage('field cannot be empty')
+    .isLength({
+      max: 50,
+    })
+    .withMessage('firstname cannot have more than 50 characters')
+    .matches(/^\S{3,}$/)
+    .withMessage('firstname cannot contain whitespaces')
+    .trim(),
+
+  check('lastname')
+    .matches(/^[a-zA-Z]+$/i)
+    .withMessage('lastname must contain only alphabets')
+    .isLength({
+      min: 1,
+    })
+    .withMessage('field cannot be empty')
+    .isLength({
+      max: 50,
+    })
+    .withMessage('lastname cannot have more than 50 characters')
+    .matches(/^\S{3,}$/)
+    .withMessage('lastname cannot contain whitespaces')
+    .trim(),
+
+  check('email').isEmail().withMessage('Please input a valid email address'),
+
+  check('password')
+    .isLength({
+      min: 5,
+    })
+    .withMessage('password must have atleast 5 characters')
+    .isLength({
+      max: 15,
+    })
+    .withMessage('password cannot contain more than 15 characters')
+    .matches(/^\S{3,}$/)
+    .withMessage('password cannot contain whitespaces')
+    .trim(),
+
+  body('confirmPassword').custom((value, { req }) => {
+    if (value != req.body.password) {
+      throw new Error('Password confirmation does not match password');
+    }
+    return true;
+  }),
+
+  (req, res, next) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      res.status(400).json({
+        status: 400,
+        error: errors.array().map((i) => i.msg),
+      });
+    }
+    next();
+  },
+];
+
+export default validateUser;
--- src/resources/user/user.route.js
+++ src/resources/user/user.route.js
@@ -1,8 +1,9 @@
 import { Router } from 'express';
 import { addUserInfo, addUserAddressInfo } from './auth.controller';
+import validateUser from '../../middlewares/validateUser';
 
 const router = Router();
 export default router;
 
-router.post('/auth/register', addUserInfo);
+router.post('/auth/register', validateUser, addUserInfo);
 router.post('/auth/register/address', addUserAddressInfo);
