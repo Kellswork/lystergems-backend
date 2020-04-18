92b4ec43cdc9f9946edb3c805e522d05e14ccc1b
kellswork <Bighead93+>
2020-04-18 19:51:43 +0100

    update test file

--- src/resources/user/spec/user.spec.js
+++ src/resources/user/spec/user.spec.js
@@ -7,35 +7,28 @@ beforeAll(async () => {
   await db.raw('truncate users cascade');
 });
 
+const user = {
+  firstname: 'kells',
+  lastname: 'leo',
+  email: 'kelsie@gmail.com',
+  password: 'pass12345',
+  confirmPassword: 'pass12345',
+};
 describe('Test for user info', () => {
   it('POST, status 201', async () => {
-    const user = {
-      firstname: 'kells',
-      lastname: 'leo',
-      email: 'kelsie@gmail.com',
-      password: 'pass12345',
-      confirmPassword: 'pass12345',
-    };
     const response = await request(app)
       .post('/api/v1/auth/register')
       .send(user);
     expect(response.statusCode).toBe(201);
     expect(response.body.message).toEqual('user created successfully');
     expect(response.body.user).toHaveProperty('id');
-    expect(response.body.user).toHaveProperty('firstname');
-    expect(response.body.user).toHaveProperty('lastname');
-    expect(response.body.user).toHaveProperty('email');
+    expect(response.body.user.firstname).toEqual('kells');
+    expect(response.body.user.lastname).toEqual('leo');
+    expect(response.body.user.email).toEqual('kelsie@gmail.com');
     expect(response.body).toHaveProperty('token');
   });
 
   it('should return 400 if email has been registered', async () => {
-    const user = {
-      firstname: 'kells',
-      lastname: 'leo',
-      email: 'kelsie@gmail.com',
-      password: 'pass12345',
-      confirmPassword: 'pass12345',
-    };
     const response = await request(app)
       .post('/api/v1/auth/register')
       .send(user);
