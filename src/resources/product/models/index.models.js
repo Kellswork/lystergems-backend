1932f8cee5e64f8b3c4c64fbf944e0591c153d30
kellswork <Bighead93+>
2020-05-01 17:52:38 +0100

    add async

--- src/resources/product/models/index.models.js
+++ src/resources/product/models/index.models.js
@@ -1,9 +1,9 @@
 import Product from './product.models';
 
-export function addProduct(product) {
+export async function addProduct(product) {
   return Product.query().insert(product).returning('*');
 }
 
-export function getProductName(name) {
+export async function getProductName(name) {
   return Product.query().where({ name });
 }
