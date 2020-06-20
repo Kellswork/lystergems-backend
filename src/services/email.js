9965ae1ac70023f6b0ed3594b43fe80d09ea97af
kells <kellogbonna@gmail.com>
2020-06-20 19:05:36 +0100

    implement email verification

--- src/services/email.js
+++ src/services/email.js
@@ -1,45 +1,57 @@
 import nodemailer from 'nodemailer';
+import Email from 'email-templates';
+import path from 'path';
+// const nodemailer = require('nodemailer');
+// const Email = require('email-templates');
+// const path = require('path');
+
 require('dotenv').config();
 
-// async..await is not allowed in global scope, must use a wrapper
-async function mailService(user, token) {
-  try {
-    // listen for token updates (if refreshToken is set)
-    const transporter = nodemailer.createTransport({
-      host: 'smtp.gmail.com',
-      port: 465,
-      secure: true,
-      auth: {
-        type: 'OAuth2',
-        // move all of this to .env file
-        user: 'kellshoppingsite@gmail.com',
-        clientId:process.env.clientId,
-        clientSecret:process.env.clientSecret ,
-        refreshToken: process.env.refreshToken,
-        accessToken: process.env.accessToken,
-        expires: 3599,
+// listen for token updates (if refreshToken is set)
+const transporter = nodemailer.createTransport({
+  host: 'smtp.gmail.com',
+  port: 465,
+  secure: true,
+  auth: {
+    type: 'OAuth2',
+    user: process.env.userEmail,
+    clientId: process.env.clientId,
+    clientSecret: process.env.clientSecret,
+    refreshToken: process.env.refreshToken,
+    accessToken: process.env.accessToken,
+    expires: 3599,
+  },
+});
+
+const email = new Email({
+  transport: transporter,
+  send: true,
+  preview: false,
+  views: {
+    options: {
+      extension: 'hbs',
+    },
+    root: __dirname,
+  },
+});
+const sendEmailConfirmation = (user, token) =>
+  email
+    .send(
+      {
+        template: path.join(__dirname, 'template'),
+        message: {
+          from: 'LysterGems <support@lystergems.com>', // sender address
+          to: user.email, // list of receivers
+        },
+        locals: {
+          fname: user.firstname,
+          lname: user.lastname,
+          token,
+        },
       },
-    });
+      console.log('email', user, token),
+    )
+    .then(() => console.log('Message sent: %s'))
+    .catch((error) => console.log(error));
 
-    // email verification url
-    const url = `http://localhost:3000/api/v1/auth/emailVerification?token=${token}`;
-    // might be moving this to the endpoint or create a function and export it
-
-    const info = await transporter.sendMail({
-      from: 'support@shoppingsite.com', // sender address
-      to: user.email, // list of receivers
-      subject: 'Verify your email address', // Subject line
-      // text: 'Hello world?', // plain text body
-      html: `<h3>Hi ${user.firstname}<h3>
-           <p> please click on the link to verify your email</p>
-           <p> ${url}</p>`, // html body
-    });
-    console.log('Message sent: %s', info.messageId);
-
-    // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
-  } catch (error) {
-    console.log(error);
-  }
-}
-
-export default mailService;
+export default sendEmailConfirmation;
