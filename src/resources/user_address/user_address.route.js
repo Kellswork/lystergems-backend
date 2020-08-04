00e6cd0e54d1ad98d13c25d9235e99c2c4af0297
Omenkish <omenkish@gmail.com>
2020-08-04 06:49:55 +0100

    Allow users to add multiple addresses

--- src/resources/user_address/user_address.route.js
+++ src/resources/user_address/user_address.route.js
@@ -1,10 +1,7 @@
 import { Router } from 'express';
 import addUserAddress from './users_address.controller';
 import { verifyAuth } from '../../middlewares/validateUserAuth';
-import {
-  validateUserAddress,
-  checkIfUserAddressExists,
-} from '../../middlewares/validateUserAddress';
+import validateUserAddress from '../../middlewares/validateUserAddress';
 
 const router = Router();
 
@@ -12,7 +9,6 @@ router.post(
   '/users/:id/address',
   verifyAuth,
   validateUserAddress,
-  checkIfUserAddressExists,
   addUserAddress,
 );
 
