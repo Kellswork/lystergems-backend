4ac11f60574aaab6346138901bc707935a2ae8e4
kells <kellogbonna@gmail.com>
2020-08-04 12:38:17 +0100

    fix merge conflict on user model

--- src/resources/order/models/order.model.js
+++ src/resources/order/models/order.model.js
@@ -42,6 +42,24 @@ export default class Order extends Model {
       },
     };
   }
+
+  static get relationMappings() {
+    // best to use a file path for ModelClass eslint won't let me call it here
+    // to know why I wanted to call it here => https://vincit.github.io/objection.js/guide/models.html#examples
+    // eslint-disable-next-line global-require
+    const User = require('../../user/models/user.model');
+    return {
+      user: {
+        // reason for the change: https://vincit.github.io/objection.js/guide/relations.html#examples
+        relation: Model.BelongsToOneRelation,
+        modelClass: User.default,
+        join: {
+          from: 'orders.user_id',
+          to: 'users.id',
+        },
+      },
+    };
+  }
 }
 
 export const STATUSES = ['pending', 'in_transit', 'delivered', 'cancelled'];
