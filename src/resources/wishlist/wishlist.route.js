cb4542d377ad330eb611e941dea9d3e7f34bbc7f
kells <kellogbonna@gmail.com>
2020-08-26 15:03:09 +0100

    add controller and route logic for adding a product to wishlist

---
+++ src/resources/wishlist/wishlist.route.js
@@ -0,0 +1,9 @@
+import { Router } from 'express';
+import { verifyAuth } from '../../middlewares/validateUserAuth';
+import { addProductToWishlist } from './wishlist.controller';
+
+const router = Router();
+
+router.post('users/:userId/wishlists', verifyAuth, addProductToWishlist);
+
+export default router;
