d5840df13e68db9af4fec89daf80950a28cf4f2c
kells <kellogbonna@gmail.com>
2020-08-27 14:47:33 +0100

    get user wishlist

--- src/resources/wishlist/wishlist.route.js
+++ src/resources/wishlist/wishlist.route.js
@@ -1,9 +1,17 @@
 import { Router } from 'express';
 import { verifyAuth } from '../../middlewares/validateUserAuth';
-import addProductToWishlist from './wishlist.controller';
+import { addProductToWishlist, getUserWishlist } from './wishlist.controller';
+import { checkUserId } from '../../middlewares/baseMiddleware';
 
 const router = Router();
 
 router.post('/users/:userId/wishlists', verifyAuth, addProductToWishlist);
+
+router.get(
+  '/users/:userId/wishlists',
+  verifyAuth,
+  checkUserId,
+  getUserWishlist,
+);
 
 export default router;
