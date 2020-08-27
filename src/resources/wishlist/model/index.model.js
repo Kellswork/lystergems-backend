b2b24de2ddb94d879522f890cbfc6a65ebc9baee
kells <kellogbonna@gmail.com>
2020-08-27 14:47:33 +0100

    get user wishlist

--- src/resources/wishlist/model/index.model.js
+++ src/resources/wishlist/model/index.model.js
@@ -1,5 +1,9 @@
 import Wishlist from './wishlist.model';
 
-export default function addToWishlist(data) {
+export async function addToWishlist(data) {
   return Wishlist.query().insert(data).returning('*');
+}
+
+export async function fetchUserWishlist(userId) {
+  return Wishlist.query().select('*').where({ user_id: userId });
 }
--- src/resources/wishlist/wishlist.controller.js
+++ src/resources/wishlist/wishlist.controller.js
@@ -1,10 +1,18 @@
 import { formatResponse } from '../../helpers/baseHelper';
-import addToWishlist from './model/index.model';
+import { addToWishlist, fetchUserWishlist } from './model/index.model';
 
-const addProductToWishlist = async (req, res) => {
+export const addProductToWishlist = async (req, res) => {
   const { id } = req.user;
   req.body.user_id = id;
   try {
+    const isProductInList = await fetchUserWishlist(id);
+    if (isProductInList.length > 0)
+      return formatResponse(
+        res,
+        { message: 'product has already been added to wishlist' },
+        400,
+      );
+
     const wishlist = await addToWishlist(req.body);
     const data = {
       wishlist,
@@ -29,4 +37,27 @@ const addProductToWishlist = async (req, res) => {
   }
 };
 
-export default addProductToWishlist;
+export const getUserWishlist = async (req, res) => {
+  const { id } = req.user;
+  req.body.user_id = id;
+  try {
+    const wishlist = await fetchUserWishlist(req.params.userId);
+    const data = {
+      wishlist,
+    };
+    return formatResponse(
+      res,
+      {
+        message: `${wishlist.length} found`,
+      },
+      200,
+      data,
+    );
+  } catch (error) {
+    return formatResponse(
+      res,
+      { error: 'could not get wishlist, please try again later' },
+      500,
+    );
+  }
+};
